da8c65803364be4d0f1f0c0a9326bd18abf21526
Siddharth Bhat <siddu.druid@gmail.com>
2020-04-21 16:41:35 +0530

    trying to get HMC working..

--- report/mcmc1d.py
+++ report/mcmc1d.py
@@ -42,8 +42,17 @@ def mh_uncorr(x0, prob, prop, iters_per_sample):
     while True:
         for i in range(iters_per_sample):
             xnext = prop(x); p = prob(x); pnext = prob(xnext)
-            r = np.random.uniform() + 1e-5;
-            if np.log(r) < min(0, np.log(pnext) - np.log(p)): x = xnext
+            r = np.random.uniform()
+            if r < min(1, pnext/p): x = xnext
+        yield xnext
+
+def mh_opt(x0, logprob, prop, iters_per_sample):
+    yield x0; x = x0;
+    while True:
+        for i in range(iters_per_sample):
+            xnext = prop(x);  lpnext = logprob(xnext); lp = logprob(x);
+            r = np.random.uniform()
+            if np.log(r) < min(0, lpnext - lp): x = xnext
         yield xnext
 
 # MH
@@ -74,8 +83,8 @@ ax[0].spines['bottom'].set_visible(False)
 ax[0].spines['left'].set_visible(False)
 
 ax[1].set_title("metropolis-hastings-simple (cumulative sum) #samples=%s"% NSAMPLES)
-ax[1].hist(xs, bins=80, cumulative=True, density=True, label='prob', linewidth=5, color=COLORSAMPLES)
-ax[1].plot(fxs, fyscum, linewidth=5, color=COLORTRUTH)
+ax[1].hist(xs, bins=200, cumulative=True, density=True, label='prob', linewidth=5, color=COLORSAMPLES)
+ax[1].plot(fxs, fyscum, linewidth=5, color=COLORTRUTH, alpha=0.5)
 ax[1].spines['top'].set_visible(False)
 ax[1].spines['right'].set_visible(False)
 ax[1].spines['bottom'].set_visible(False)
@@ -109,8 +118,8 @@ ax[0].spines['bottom'].set_visible(False)
 ax[0].spines['left'].set_visible(False)
 
 ax[1].set_title("metropolis-hastings-uncorrelate simple (cumulative sum) #samples=%s | #iters/sample: %s"% (NSAMPLES, ITERS_PER_SAMPLE))
-ax[1].hist(xs, bins=80, cumulative=True, density=True, label='prob', linewidth=5, color=COLORSAMPLES)
-ax[1].plot(fxs, fyscum, linewidth=5, color=COLORTRUTH)
+ax[1].hist(xs, bins=200, cumulative=True, density=True, label='prob', linewidth=5, color=COLORSAMPLES)
+ax[1].plot(fxs, fyscum, linewidth=5, color=COLORTRUTH, alpha=0.5)
 ax[1].spines['top'].set_visible(False)
 ax[1].spines['right'].set_visible(False)
 ax[1].spines['bottom'].set_visible(False)
@@ -118,39 +127,4 @@ ax[1].spines['left'].set_visible(False)
 fig_size = plt.gcf().get_size_inches() #Get current size
 plt.gcf().set_size_inches(2.0 * fig_size) 
 plt.savefig("mcmc-mh-uncorr-1d-exp.png")
-plt.show()
-
-
-def hmc(q0, logprob, logprobgrad, iters_per_sample, dt):
-    yield q0; q = q0
-    while True:
-        # number of simulation steps
-        p = np.random.normal(0, 1)
-        for i in range(iters_per_sample):
-            def hdp(p, q): return 0.5*p*p
-            def hdq(p, q): return logprobgrad(q)
-            (p, q) = leapfrog(hdp, hdq, p, q, dt)
-        yield q
-
-# HMC
-NSAMPLES = 1000
-ITERS_PER_SAMPLE = 10
-DT = 1e-2
-xs = list(itertools.islice(hmc(3, exp, expgrad, ITERS_PER_SAMPLE, DT), NSAMPLES))
-ys = [exp(x) for x in xs]
-plt.rcParams.update({'font.size': 10, 'font.family':'monospace'})
-fig, ax = plt.subplots(2, 1)
-ax[0].plot(xs, ys, 'x', label='prob',
-        linewidth=5, color=COLORSAMPLES, markersize=4.0)
-
-legend = plt.legend(frameon=False)
-ax[0].set_title("metropolis-hastings: #samples=%s | #iters per sample=%s" % (NSAMPLES, ITERS_PER_SAMPLE))
-ax[0].spines['top'].set_visible(False)
-ax[0].spines['right'].set_visible(False)
-ax[0].spines['bottom'].set_visible(False)
-ax[0].spines['left'].set_visible(False)
-
-fxs = np.arange(-3, 3, (3 - (-3)) / 30.0); fys = np.cumsum([exp(x) for x in fxs]); fys = fys / np.max(fys)
-ax[1].hist(xs, bins=80, cumulative=True, density=True, label='prob', linewidth=5, color=COLORTRUTH)
-ax[1].plot(fxs, fys)
 plt.show()
